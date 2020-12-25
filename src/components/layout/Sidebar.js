58e40cf6752f5846c907de486882f9d4f295222a
outhanchazima <outhan254@gmail.com>
2020-12-25 17:30:38 +0300

    sidebar styles

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -4,35 +4,34 @@ export class Sidebar extends Component {
     render() {
         return (
             <div className="side-bar">
-                <nav>
+                <nav className="side-nav">
                     <div className="nav-brand" >
-                        <i className="bx bx-layer nav-toggle" id="nav-toggle"></i>
+                        <i className="bx bx-folder"></i>
                         <span className="nav-logo-name">AccessAfya</span>
                     </div>
                     <div className="nav-list">
                         <a href="##" className="nav-link">
-                            <box-icon type='solid' name='dashboard'></box-icon>
+                            <i className="bx bxs-dashboard nav-icon"></i>
                             <span className="nav-name" >Dashboard</span>
                         </a>
                         <a href="##" className="nav-link">
-                            <box-icon name='folder' ></box-icon>
+                            <i className="bx bx-folder nav-icon"></i>
                             <span className="nav-name" >Documents</span>
                         </a>
                         <a href="##" className="nav-link">
-                            <box-icon name='info-circle' ></box-icon>
+                            <i className="bx bx-info-circle nav-icon" ></i>
                             <span className="nav-name" >Patients</span>
                         </a>
                         <a href="##" className="nav-link">
-                            <box-icon name='message-detail' ></box-icon>
+                            <i className= "bx bx-message-detail nav-icon"></i>
                             <span className="nav-name" >Messages</span>
                         </a>
                     </div>
                     <a href="##" className="nav-link">
-                        <box-icon type='solid' name='cookie'></box-icon>
+                        <i className="bx bx-cookie nav-icon"></i>
                         <span className="nav-name" >Settings</span>
                     </a>
                 </nav>
-                
             </div>
         )
     }
